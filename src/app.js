00c2fe6ea9512934b58313f144a87e4bd05bd1d9
Martina <martuvissani@gmail.com>
2025-08-15 06:02:50 -0300

    actualizo el puerto

--- src/app.js
+++ src/app.js
@@ -26,7 +26,8 @@ app.use("/", indexRouter)
 
 // SERVIDOR
 
-app.listen("3000", function() 
-{
-console.log("Servidor corriendo en el puerto 3000!")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}!`);
+});
